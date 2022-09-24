5ded7432404c22a9061fe8df36fc1e8004262aff
Anna <eekolegov@mail.ru>
2022-09-24 23:29:18 +0300

    Zadacha23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,13 @@
+﻿void zadacha23()
+{
+    int number1=new Random().Next(1,10);
+    Console.WriteLine($"число:{number1}");
+    for (int index=1; index<=number1; index++)
+    {
+        Console.WriteLine ($"{index}^3={Math.Pow(index, 3)}");
+    }
+   
+    
+}
+
+zadacha23();
