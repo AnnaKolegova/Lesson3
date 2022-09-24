535480a774eea189f06c2c937fae49d50f3ac00d
Anna <eekolegov@mail.ru>
2022-09-24 22:00:58 +0300

    Zadacha19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,17 @@
+﻿void zadacha19()
+{
+    int number1=new Random().Next(10000,100000);
+    Console.WriteLine($"число:{number1}");
+    if ((number1/10000==number1%10) && (number1/1000%10==number1%100/10))
+    {
+        Console.WriteLine("число - палиндром");
+    }
+    else
+    {
+       
+        Console.WriteLine("число не палиндром");
+    }
+    
+}
+
+zadacha19();
